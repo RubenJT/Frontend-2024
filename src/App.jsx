43cade02cd86a4fcfc7aab000d0410ad13168f75
RubenJT <144279622+RubenJT@users.noreply.github.com>
2024-02-15 20:45:15 -0600

    Buttom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react"
+
+
+
 
 
 function App() {
-
+  const [count, setcount] = useState (0)
+  const handleButtomIncrement =  () => {setcount (count+1)}
+  const handleButtomdecrement =  () => {setcount (count-1)}
+  const handleButtom0 =  () => {setcount(0)}
+  
 
   return (
-<div><h1>Bootstrap</h1><hr /></div>
+  
+    <div> <h1>buttom</h1> <hr /> 
+     
+    <h1>{count}</h1>
+  <button onClick={handleButtomIncrement}>incrementar</button>
+
+  <button onClick={handleButtom0}>reinciar</button>
+
+  <button onClick={handleButtomdecrement}>decremntar</button>
+
+  <button onAbortCapture= {()=> {
+    
+
+    return{
+
+    }
+
+  }}>guardar numero</button>
+
+  </div>
   )
 }
 
